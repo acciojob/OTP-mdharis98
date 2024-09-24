d537a5c897e103a87197b0202d056f2b046d38ea
Mohammad Haris <112751385+mdharis98@users.noreply.github.com>
2024-09-25 00:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+    code.addEventListener('keydown', (e) => {
+        if (e.key >= 0 && e.key <= 9) {
+            codes[index].value = ''; // Clear the input before typing
+            setTimeout(() => {
+                if (index < codes.length - 1) {
+                    codes[index + 1].focus();
+                }
+            }, 10);
+        } else if (e.key === 'Backspace') {
+            setTimeout(() => {
+                if (index > 0) {
+                    codes[index - 1].focus();
+                }
+            }, 10);
+        }
+    });
+});
